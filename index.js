24b0f3d22d5e3a6da2e43b1608449a6cd59159d8
Luis <luisabc132@gmail.com>
2021-06-11 11:40:43 +0200

    Espacio de trabajo listo

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const { json, urlencoded } = require("body-parser");
+const morgan = require("morgan");
+const cors = require("cors");
+const path = require("path");
+require('dotenv').config()
+const users  = require('./src/users/users.router');
+const mongoose = require("mongoose");
+const options = { useNewUrlParser: true, useUnifiedTopology: true };
+const mongo = mongoose.connect(
+    process.env.DB_HOST,
+    options
+  );
+  mongo.then(() =>{
+    console.log("mongo listo Papito");
+  });
+
+
+global.appRoot = path.resolve(__dirname);
+
+const app = express();
+
+app.use(cors());
+app.use(json());
+app.use(urlencoded({ extended: true }));
+app.use(morgan("dev"));
+app.disable("x-powered-by");
+
+app.get("/test", (request, response) => {
+  response.send("Soy un test!");
+});
+
+const start = async () => {
+  try {
+    app.listen(5001, () => {
+      console.log(`REST API on http://localhost:5001/api`);
+    });
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+start();
