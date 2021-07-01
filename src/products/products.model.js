8f930133d8b2715cc0ca51649c9649149bbc2f1f
Clàudia <claudia.martinez081996@gmail.com>
2021-07-01 13:25:17 +0200

    coding improvement + schema img changed

--- src/products/products.model.js
+++ src/products/products.model.js
@@ -1,59 +1,51 @@
 const mongoose = require("mongoose");
-require('mongoose-type-url');
-require ("../users/users.model");
+require("mongoose-type-url");
+require("../users/users.model");
 
 const ProductsSchema = new mongoose.Schema({
-    name: String,
-    description: String,
-    startPrice: Number,
-    images: [{type: mongoose.SchemaTypes.Url}],
-    sellerId: {type: mongoose.Schema.Types.ObjectId, ref: "user"},
-    state: {type: mongoose.Schema.Types.ObjectId, ref: "bids"}, 
-    productState: String,
-    timestramp: {type: Date, default: Date.now},
-    bids: [{type: mongoose.Schema.Types.ObjectId, ref: "bids"}],
-    finalPrice: {type: mongoose.Schema.Types.ObjectId, ref: "bids"}, 
+  name: { type: mongoose.Schema.Types.String },
+  description: { type: mongoose.Schema.Types.String },
+  startPrice: { type: mongoose.Schema.Types.Number },
+  images: [{ type: mongoose.Schema.Types.String }],
+  sellerId: { type: mongoose.Schema.Types.ObjectId, ref: "user" },
+  state: { type: mongoose.Schema.Types.ObjectId, ref: "bids" },
+  productState: { type: mongoose.Schema.Types.String },
+  timestramp: { type: Date, default: Date.now },
+  bids: [{ type: mongoose.Schema.Types.ObjectId, ref: "bids" }],
+  finalPrice: { type: mongoose.Schema.Types.ObjectId, ref: "bids" },
 });
 
-const ProductsModel = mongoose.model('products', ProductsSchema);
+const ProductsModel = mongoose.model("products", ProductsSchema);
 
 const getAll = async () => {
-    const products = await ProductsModel.find();
-    return products;
-}
+  const products = await ProductsModel.find();
+  return products;
+};
 
 const create = async (product) => {
-    const productCreated = await ProductsModel.create(product);
-    return productCreated;
-}
+  const productCreated = await ProductsModel.create(product);
+  return productCreated;
+};
 
 const getById = async (id) => {
-    const productById = await ProductsModel.findById(id);
-    return productById;
-}
+  const productById = await ProductsModel.findById(id);
+  return productById;
+};
 
- const searchWord = async (query) => {
-    const products = await ProductsModel.findOne(query);
-    return products;
-}
-
+const searchWord = async (query) => {
+  const products = await ProductsModel.findOne(query);
+  return products;
+};
 
 const updateById = async (id, body) => {
-    const updateProductById = await ProductsModel.findByIdAndUpdate(id, body)
-    return updateProductById;
-} 
+  const updateProductById = await ProductsModel.findByIdAndUpdate(id, body);
+  return updateProductById;
+};
 
 module.exports = {
-    getAll,
-    create, 
-    getById,
-   searchWord,
-    updateById,
-}
-
-
-
-
-
-
-
+  getAll,
+  create,
+  getById,
+  searchWord,
+  updateById,
+};
