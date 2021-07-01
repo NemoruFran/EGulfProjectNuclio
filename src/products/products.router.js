5b277bae5465a749d16c7c7875214428cbcd93e0
Clàudia <claudia.martinez081996@gmail.com>
2021-07-01 13:29:38 +0200

    url image validation

--- src/products/products.router.js
+++ src/products/products.router.js
@@ -1,34 +1,30 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const productsController = require ("./products.controller");
-const jwt = require ("jsonwebtoken"); 
-const {body} = require("express-validator");
+const productsController = require("./products.controller");
+const jwt = require("jsonwebtoken");
+const { body } = require("express-validator");
 
 const middleware = async (req, res, next) => {
-    if (!req.headers.authorization) {
-        return res.status(401).json("Forbidden.");
-    }
-    const token = req.headers.authorization.split(' ')[1];
-  
-    jwt.verify(token, process.env.TOKEN_SECRET, function(err) {
-        if (err) {
-          return res.status(401).json(err)
-        }
-        return next();
-    })
+  if (!req.headers.authorization) {
+    return res.status(401).json("Forbidden.");
   }
+  const token = req.headers.authorization.split(" ")[1];
+
+  jwt.verify(token, process.env.TOKEN_SECRET, function (err) {
+    if (err) {
+      return res.status(401).json(err);
+    }
+    return next();
+  });
+};
 
 router
-  .route ("/")
-    .get (productsController.all)
-    .post (middleware, productsController.create)
+  .route("/")
+  .get(productsController.all)
+  .post(body("images").isURL(), middleware, productsController.create);
 
-router
-  .route ("/:id")
-    .get(productsController.getOne)
-  
-router
-  .route('/search/:text')
-    .get(productsController.search) 
+router.route("/:id").get(productsController.getOne);
+
+router.route("/search/:text").get(productsController.search);
 
 module.exports = router;
